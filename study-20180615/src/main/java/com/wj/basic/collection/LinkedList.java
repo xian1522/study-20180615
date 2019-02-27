a0dcf9715d6070d11966080a98678599def76d46
xian1522 <xian1522@126.com>
2019-02-28 00:00:49 +0800

    LinkedList

---
+++ study-20180615/src/main/java/com/wj/basic/collection/LinkedList.java
@@ -0,0 +1,42 @@
+package com.wj.basic.collection;
+
+public class LinkedList<E> {
+	
+	Node<E> last;
+	Node<E> first;
+	
+	private int size;
+	
+	public LinkedList() {
+		
+	}
+	
+	public boolean add(E e) {
+		linkLast(e);
+		return true;
+	}
+	
+	void linkLast(E e) {
+		Node<E> l = last;
+		Node<E> newNode = new Node<E>(l,e,null);
+		last = newNode;
+		if(l==null) {
+			first = newNode;
+		}else {
+			last.next = newNode;
+		}
+		size++;
+	}
+	
+	private static class Node<E>{
+		E item;
+		Node<E> prev;
+		Node<E> next;
+		
+		Node(Node<E> prev,E item, Node<E>next){
+			this.prev = prev;
+			this.item = item;
+			this.next = next;
+		}
+	}
+}
